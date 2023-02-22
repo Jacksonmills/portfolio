ce3f08272badebc21a9f81bef4706a818f046525
Jacksonmills <jacksonemills@gmail.com>
2023-02-21 22:28:38 -0600

    testing newer server stylesheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,37 +1,50 @@
-import * as React from 'react';
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { CSSProperties, ServerStyleSheet } from 'styled-components';
+interface PageDocumentProps {
+  __NEXT_DATA__: {
+    props: {
+      appProps: any;
+    };
+  };
 }
+export default function PageDocument({
+  __NEXT_DATA__: {
+    props: { appProps },
+  },
+}: PageDocumentProps) {
+  return (
+    <Html lang={appProps.lang}>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+PageDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const styledComponentsSheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => {
+          return styledComponentsSheet.collectStyles(<App {...props} />);
+        },
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    initialProps.styles = [
+      initialProps.styles,
+      styledComponentsSheet.getStyleElement(),
+    ];
+    return initialProps;
+  } finally {
+    styledComponentsSheet.seal();
+  }
+};
