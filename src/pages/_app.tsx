d65af33c5cbeb4e7dd5e294a06053988317a8124
Jacksonmills <jacksonemills@gmail.com>
2023-02-25 08:11:30 -0600

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import GlobalStyles from '@/components/GlobalStyles';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <GlobalStyles />
+      <Analytics />
       <div id='modal'></div>
     </>
   );
